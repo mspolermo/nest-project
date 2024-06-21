6b60663c954ab765152206dab33a9584ddae564b
mspolermo <mspolermo@gmail.com>
2024-06-21 18:05:23 +0500

    controllers, providers added; connected to PostgeSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from "@nestjs/common";
+import { AppController } from "./app.conroller";
+import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
 
 @Module({
-    
+    controllers: [AppController],
+    providers: [AppService],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'root',
+            database: 'nest-project',
+            models: [],
+            autoLoadModels: true
+        }),
+    ]
 })
-export class AppModule {
-
-}
+export class AppModule {}
