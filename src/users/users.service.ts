57ee90260939629c9df5b2cb46a434e9be558a13
mspolermo <mspolermo@gmail.com>
2024-06-23 17:21:04 +0500

    users model added, users table created

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './users.model';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@InjectModel(User) private userRepository: typeof User) {
+
+    }
+
+    async createUser() {
+
+    }
+
+    async geatAllUsers() {
+
+    }
+}
