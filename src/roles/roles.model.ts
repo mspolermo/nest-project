8e42400719d3c32674e441de0c88fc18b546b48d
mspolermo <mspolermo@gmail.com>
2024-06-26 18:07:57 +0500

    connect user and roles tables via many-to-many completed

--- src/roles/roles.model.ts
+++ src/roles/roles.model.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { BelongsToMany, Column, DataType, Model, Table } from "sequelize-typescript";
+import { User } from "src/users/users.model";
+import { UserRoles } from "./user-roles.model";
 
 interface RoleCreationAttrs {
     value: string;
@@ -20,4 +22,6 @@ export class Role extends Model<Role, RoleCreationAttrs > {
     @Column({type: DataType.STRING, allowNull: false})
     description: string;
 
+    @BelongsToMany(() => User, () => UserRoles)
+    users: User[]
 }
